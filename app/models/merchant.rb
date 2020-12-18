39a0a72c1415436fde9d701f310bf4963da83507
Aidan Murray <aidanhansm@gmail.com>
2020-12-18 00:01:15 -0700

    Add and update models for statistics

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
+  has_many :invoices
+  has_many :transactions, through: :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :customers, through: :invoices
 
   def attributes
     {
