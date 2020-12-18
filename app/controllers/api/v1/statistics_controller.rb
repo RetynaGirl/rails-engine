07f0b8f2d61447f26b8a99fe8922375b36d20f7f
Aidan Murray <aidanhansm@gmail.com>
2020-12-18 04:10:29 -0700

    Set the date range to be inclusive on both ends

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::StatisticsController < ApplicationController
   def revenue_date_range
     start_date = Date.new(*params[:start].split(/[^0-9]/).map(&:to_i))
-    end_date = Date.new(*params[:end].split(/[^0-9]/).map(&:to_i))
+    end_date = Date.new(*params[:end].split(/[^0-9]/).map(&:to_i)).next_day
 
     data = InvoiceItem.where(created_at: start_date..end_date).sum("quantity * unit_price").to_f
 
