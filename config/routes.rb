6d4cc7252a7f5623ba113d1d056bfd4347219189
Aidan Murray <aidanhansm@gmail.com>
2020-12-17 21:18:53 -0700

    Update routes through requirement 3b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [:index, :show, :create, :update, :destroy]
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+
+      namespace :items do
+        get '/:id/merchants', to: 'merchants#index'
+      end
+
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+      end
+    end
+  end
 end
