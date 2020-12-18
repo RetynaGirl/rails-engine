40983d68bd59721b56713c13b3f2fb5cbf3a69e7
Aidan Murray <aidanhansm@gmail.com>
2020-12-18 03:53:37 -0700

    Update CSV data intake

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
-
 require 'csv'
-
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
@@ -13,8 +11,8 @@ require 'csv'
 # - put the "rails-engine-development.pgdump" file in db/data/
 # - put the "items.csv" file in db/data/
 
-cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails-engine_development db/data/rails-engine-development.pgdump"
-puts "Loading PostgreSQL Data dump into local database with command:"
+cmd = 'pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails-engine_development db/data/rails-engine-development.pgdump'
+puts 'Loading PostgreSQL Data dump into local database with command:'
 puts cmd
 system(cmd)
 
@@ -23,8 +21,11 @@ system(cmd)
 data = CSV.parse(File.new('/Users/aidan/turing/3mod/projects/rails_engine_pack/rails-engine/db/data/items.csv'), headers: true)
 
 data.each do |row|
-  # require 'pry'; binding.pry
-  Item.create!(row.to_h)
+  hash = row.to_h
+
+  hash['unit_price'] = hash['unit_price'].to_i / 100.0
+
+  Item.create!(hash)
 end
 # - Add code to reset the primary key sequences on all 6 tables (merchants, items, customers, invoices, invoice_items, transactions)
 
