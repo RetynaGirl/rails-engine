100f0aaed10d42d4a26a8e7a86da24e83711d895
Aidan Murray <aidanhansm@gmail.com>
2020-12-17 22:31:04 -0700

    Add item find route

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,17 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :create, :update, :destroy]
-      resources :merchants, only: [:index, :show, :create, :update, :destroy]
-
       namespace :items do
         get '/:id/merchants', to: 'merchants#index'
+        get '/find', to: 'search#show'
       end
 
       namespace :merchants do
         get '/:id/items', to: 'items#index'
       end
+
+      resources :items, only: [:index, :show, :create, :update, :destroy]
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
