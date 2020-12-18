02c0b32c3808f37a9fb9fbf784b3702f3d5aed65
Aidan Murray <aidanhansm@gmail.com>
2020-12-18 07:16:14 -0700

    Add quantity default to most revenue

--- app/controllers/api/v1/merchants/statistics_controller.rb
+++ app/controllers/api/v1/merchants/statistics_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::Merchants::StatisticsController < ApplicationController
   def most_revenue
+    quantity = params[:quantity] || 10
+
     # merchants = Merchant.find_by_sql('SELECT merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS sum_invoice_items_quantity_all_invoice_items_unit_price FROM "merchants" INNER JOIN "invoices" ON "invoices"."merchant_id" = "merchants"."id" INNER JOIN "invoice_items" ON "invoice_items"."invoice_id" = "invoices"."id" INNER JOIN "transactions" ON "transactions"."invoice_id" = "invoices"."id" WHERE (invoices.status = ' + "'shipped'" + ') AND (transactions.result = ' + "'success'" + ') GROUP BY merchants.id ORDER BY sum_invoice_items_quantity_all_invoice_items_unit_price DESC LIMIT ', + params[:quantity].to_s)
 
     # merchants = Merchant.select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS sum_invoice_items_quantity_all_invoice_items_unit_price')
@@ -19,7 +21,7 @@ class Api::V1::Merchants::StatisticsController < ApplicationController
                                                        WHERE (invoices.status = ' + "'shipped'" + ') AND (transactions.result = ' + "'success'" + ')
                                                        GROUP BY merchants.id
                                                        ORDER BY sum_invoice_items_quantity_all_invoice_items_unit_price DESC
-                                                       ' + "LIMIT #{params[:quantity]}")
+                                                       ' + "LIMIT #{quantity}")
     begin
       merchants = merchants_data.map do |merchant_data|
         merchant = Merchant.new(merchant_data.except('sum_invoice_items_quantity_all_invoice_items_unit_price'))
